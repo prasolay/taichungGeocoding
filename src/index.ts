0086badb9de9109d5590cc32fcee887a3a338489
prasolay2020 <prasolay2020@gmail.com>
2024-11-07 20:45:25 +0800

    準備要在express中載入所有資料夾檔案

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,8 @@ const app = express();
 
 // 動態選擇環境變數的檔案
 dotenv.config({ path: path.resolve(__dirname, `./environments/${ process.env.NODE_ENV }.env`) });
-console.log(__dirname);
-console.log(process.env.NODE_ENV);
-console.log(process.env.PORT);
-
 app.get('/', (req, res, next) => {
-    res.send('Hello, World!!進入到index.ts中!');
+    res.send(render('index.html'));
 });
-
 
 app.listen(process.env.PORT, () => console.log(`http server is running at port ${ process.env.PORT }.`));
