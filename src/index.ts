4802d4d18cb1523f02df09ee5d860326552d086b
prasolay2020 <prasolay2020@gmail.com>
2024-11-07 19:52:17 +0800

    更新要被忽略的檔案

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
 
 const app = express();
 
+// 動態選擇環境變數的檔案
+dotenv.config({ path: path.resolve(__dirname, `/environments/${ process.env.NODE_ENV }.env`) });
+console.log(__dirname);
+
 app.get('/', (req, res, next) => {
-    res.send('Hello, World!!進入到index.ts中！');
+    res.send('Hello, World!!進入到index.ts中!');
+    res.send('你好！');
 });
 
-app.listen(3000, () => console.log('http server is running at port 3000.'));
+
+app.listen(process.env.PORT, () => console.log(`http server is running at port ${ process.env.PORT }.`));
