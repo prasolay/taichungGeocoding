dcc50cad9730ccfaacc4b8f6d6a4d363d607cca6
prasolay2020 <prasolay2020@gmail.com>
2024-11-15 16:54:25 +0800

    準備根據gpt進行除錯

--- gulpfile.js
+++ gulpfile.js
@@ -23,24 +23,49 @@ gulp.task("copy-html", function () {
   return gulp.src(paths.pages).pipe(gulp.dest("public"));
 });
 
+// //初始化瀏覽器環境變數
+// const watchedBrowserify = watchify(
+//   browserify({
+//     basedir: ".",
+//     debug: true,
+//     entries: ["src/js/main.ts"],
+//     cache: {},
+//     packageCache: {},
+//   })
+// ).plugin(tsify);
+
 //豪呈程式碼
-parallelList.push("js");
-gulp.task("js", function () {
-  var js = ["./src/js/*.js"];
+// parallelList.push("js");
+// gulp.task("js", function () {
+//   var ts = ["./src/js/*.ts"];
 
-  if (process.env.NODE_ENV === "production") {
-    gulp
-      .src(js)
-      .pipe(uglify({ output: { comments: false } }))
-      .pipe(gulp.dest("./public/js/"));
-  } else {
-    gulp.src(js).pipe(gulp.dest("./public/js/"));
-  }
+//   if (process.env.NODE_ENV === "production") {
+//     gulp
+//       .src(ts)
+//       // .plugin(tsify)
+//       // .pipe(source("bundle.js"))
+//       // .pipe(uglify({ output: { comments: false } }))
+//       .pipe(gulp.dest("./public/js/"));
+//   } else {
+//     gulp
+//       .src(ts)
+//       // .plugin(tsify)
+//       // .bundle()
+//       // .pipe(source("bundle.js"))
+//       .pipe(gulp.dest("./public/js/"));
+//   }
 
-  return new Promise((resolve, reject) => {
-    resolve("js success");
-  });
-});
+//   return new Promise((resolve, reject) => {
+//     resolve("js success");
+//   });
+// });
+
+// function bundle() {
+//   return watchedBrowserify
+//     .bundle()
+//     .pipe(source("bundle.js"))
+//     .pipe(gulp.dest("dist"));
+// }
 
 parallelList.push("css");
 gulp.task("css", function () {
@@ -68,16 +93,53 @@ gulp.task("css", function () {
 parallelList.push("copy-html");
 gulp.task("html", function () {
   var html = ["./src/*.html"];
-  gulp.src(html).pipe(gulp.dest("public"));
-});
+  gulp.src(html).pipe(gulp.dest("./public"));
 
-gulp.task("default", gulp.parallel(parallelList), "start-sever");
+  return new Promise((resolve, reject) => {
+    resolve("html success");
+  });
+});
 
 //init sever
 gulp.task("start-server", function (cb) {
-  exec("node server.ts", function (err, stdout, stderr) {
+  exec("node server.js", function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
   });
 });
+
+gulp.task(
+  "default",
+  gulp.parallel(parallelList),
+  function () {
+    // var ts = ["./src/js/*.ts"];
+    if (process.env.NODE_ENV === "production") {
+      return browserify({
+        basedir: ".",
+        debug: true,
+        cache: {},
+        entries: ["src/js/index.ts"],
+        packageCache: {},
+      })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source("bundle.js"))
+        .pipe(uglify({ output: { comments: false } }))
+        .pipe(gulp.dest("./public/js/"));
+    } else {
+      return browserify({
+        basedir: ".",
+        debug: true,
+        cache: {},
+        entries: ["src/js/index.ts"],
+        packageCache: {},
+      })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source("bundle.js"))
+        .pipe(gulp.dest("./public/js/"));
+    }
+  },
+  "start-server"
+);
