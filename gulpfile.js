5be81f27ad0d419a7eda43df5163c13c64a2aa99
prasolay2020 <prasolay2020@gmail.com>
2024-11-15 17:32:56 +0800

    準備根據demo程式碼進行大修

--- gulpfile.js
+++ gulpfile.js
@@ -16,11 +16,10 @@ const parallelList = [];
 
 //測適用
 console.log(process.env.NODE_ENV);
-debugger;
 
 //init html
 gulp.task("copy-html", function () {
-  return gulp.src(paths.pages).pipe(gulp.dest("public"));
+  return gulp.src("./src/*.html").pipe(gulp.dest("./public"));
 });
 
 // //初始化瀏覽器環境變數
@@ -67,79 +66,97 @@ gulp.task("copy-html", function () {
 //     .pipe(gulp.dest("dist"));
 // }
 
-parallelList.push("css");
-gulp.task("css", function () {
-  var css = ["./src/css/*.scss"];
+// parallelList.push("css");
+// gulp.task("css", function () {
+//   var css = ["./src/css/*.scss"];
 
-  if (process.env.NODE_ENV === "production") {
-    gulp
-      .src(css)
-      .pipe(sass().on("error", sass.logError))
-      .pipe(minifyCss())
-      .pipe(gulp.dest("./public/css"));
-  } else {
-    gulp
-      .src(css)
-      .pipe(sass().on("error", sass.logError))
-      .pipe(gulp.dest("./public/css"));
-  }
+//   if (process.env.NODE_ENV === "production") {
+//     gulp
+//       .src(css)
+//       .pipe(sass().on("error", sass.logError))
+//       .pipe(minifyCss())
+//       .pipe(gulp.dest("./public/css"));
+//   } else {
+//     gulp
+//       .src(css)
+//       .pipe(sass().on("error", sass.logError))
+//       .pipe(gulp.dest("./public/css"));
+//   }
 
-  return new Promise((resolve, reject) => {
-    resolve("css success");
-  });
-});
+//   return new Promise((resolve, reject) => {
+//     resolve("css success");
+//   });
+// });
 
-//處理html檔案
-parallelList.push("copy-html");
-gulp.task("html", function () {
-  var html = ["./src/*.html"];
-  gulp.src(html).pipe(gulp.dest("./public"));
+// //處理html檔案
+// parallelList.push("copy-html");
+// gulp.task("copy-html", function () {
+//   var html = ["./src/*.html"];
+//   gulp.src(html).pipe(gulp.dest("./public"));
 
-  return new Promise((resolve, reject) => {
-    resolve("html success");
-  });
-});
+//   return new Promise((resolve, reject) => {
+//     resolve("html success");
+//   });
+// });
 
-//init sever
-gulp.task("start-server", function (cb) {
-  exec("node server.js", function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-});
+// //init sever
+// gulp.task("start-server", function (cb) {
+//   exec("node server.js", function (err, stdout, stderr) {
+//     console.log(stdout);
+//     console.log(stderr);
+//     cb(err);
+//   });
+// });
 
+//網路範本程式碼
 gulp.task(
   "default",
-  gulp.parallel(parallelList),
-  function () {
-    // var ts = ["./src/js/*.ts"];
-    if (process.env.NODE_ENV === "production") {
-      return browserify({
-        basedir: ".",
-        debug: true,
-        cache: {},
-        entries: ["src/js/index.ts"],
-        packageCache: {},
-      })
-        .plugin(tsify)
-        .bundle()
-        .pipe(source("bundle.js"))
-        .pipe(uglify({ output: { comments: false } }))
-        .pipe(gulp.dest("./public/js/"));
-    } else {
-      return browserify({
-        basedir: ".",
-        debug: true,
-        cache: {},
-        entries: ["src/js/index.ts"],
-        packageCache: {},
-      })
-        .plugin(tsify)
-        .bundle()
-        .pipe(source("bundle.js"))
-        .pipe(gulp.dest("./public/js/"));
-    }
-  },
-  "start-server"
+  gulp.series(gulp.parallel("copy-html"), function () {
+    return browserify({
+      basedir: ".",
+      debug: true,
+      entries: ["./src/js/main.ts"],
+      cache: {},
+      packageCache: {},
+    })
+      .plugin(tsify)
+      .bundle()
+      .pipe(source("bundle.js"))
+      .pipe(gulp.dest("./public/js"));
+  })
 );
+
+// gulp.task(
+//   "default",
+//   gulp.parallel(parallelList),
+//   function () {
+//     // var ts = ["./src/js/*.ts"];
+//     if (process.env.NODE_ENV === "production") {
+//       return browserify({
+//         basedir: ".",
+//         debug: true,
+//         cache: {},
+//         entries: ["src/js/index.ts"],
+//         packageCache: {},
+//       })
+//         .plugin(tsify)
+//         .bundle()
+//         .pipe(source("bundle.js"))
+//         .pipe(uglify({ output: { comments: false } }))
+//         .pipe(gulp.dest("./public/js/"));
+//     } else {
+//       return browserify({
+//         basedir: ".",
+//         debug: true,
+//         cache: {},
+//         entries: ["src/js/index.ts"],
+//         packageCache: {},
+//       })
+//         .plugin(tsify)
+//         .bundle()
+//         .pipe(source("bundle.js"))
+//         .pipe(gulp.dest("./public/js/"));
+//     }
+//   },
+//   "start-server"
+// );
