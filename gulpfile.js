854c81c682bd0338a67df3362ca99f51233aa346
prasolay2020 <prasolay2020@gmail.com>
2024-11-15 17:00:18 +0800

    Revert "準備根據gpt進行除錯"
    This reverts commit dcc50cad9730ccfaacc4b8f6d6a4d363d607cca6.

--- gulpfile.js
+++ gulpfile.js
@@ -23,79 +23,23 @@ gulp.task("copy-html", function () {
   return gulp.src(paths.pages).pipe(gulp.dest("public"));
 });
 
-// //初始化瀏覽器環境變數
-// const watchedBrowserify = watchify(
-//   browserify({
-//     basedir: ".",
-//     debug: true,
-//     entries: ["src/js/main.ts"],
-//     cache: {},
-//     packageCache: {},
-//   })
-// ).plugin(tsify);
-
 //豪呈程式碼
-// parallelList.push("js");
-// gulp.task("js", function () {
-//   var ts = ["./src/js/*.ts"];
+parallelList.push("js");
+gulp.task("js", function () {
+  var js = ["./src/js/*.js"];
 
-//   if (process.env.NODE_ENV === "production") {
-//     gulp
-//       .src(ts)
-//       // .plugin(tsify)
-//       // .pipe(source("bundle.js"))
-//       // .pipe(uglify({ output: { comments: false } }))
-//       .pipe(gulp.dest("./public/js/"));
-//   } else {
-//     gulp
-//       .src(ts)
-//       // .plugin(tsify)
-//       // .bundle()
-//       // .pipe(source("bundle.js"))
-//       .pipe(gulp.dest("./public/js/"));
-//   }
-
-//   return new Promise((resolve, reject) => {
-//     resolve("js success");
-//   });
-// });
-
-// function bundle() {
-//   return watchedBrowserify
-//     .bundle()
-//     .pipe(source("bundle.js"))
-//     .pipe(gulp.dest("dist"));
-// }
-
-//gpt程式碼
-gulp.task("bundle-js", function () {
   if (process.env.NODE_ENV === "production") {
-    return browserify({
-      basedir: ".",
-      debug: true,
-      cache: {},
-      entries: ["src/js/index.ts"],
-      packageCache: {},
-    })
-      .plugin(tsify)
-      .bundle()
-      .pipe(source("bundle.js"))
-      .pipe(buffer()) // 需要 buffer 來使用 gulp-uglify
+    gulp
+      .src(js)
       .pipe(uglify({ output: { comments: false } }))
       .pipe(gulp.dest("./public/js/"));
   } else {
-    return browserify({
-      basedir: ".",
-      debug: true,
-      cache: {},
-      entries: ["src/js/index.ts"],
-      packageCache: {},
-    })
-      .plugin(tsify)
-      .bundle()
-      .pipe(source("bundle.js"))
-      .pipe(gulp.dest("./public/js/"));
+    gulp.src(js).pipe(gulp.dest("./public/js/"));
   }
+
+  return new Promise((resolve, reject) => {
+    resolve("js success");
+  });
 });
 
 parallelList.push("css");
@@ -124,16 +68,14 @@ gulp.task("css", function () {
 parallelList.push("copy-html");
 gulp.task("html", function () {
   var html = ["./src/*.html"];
-  gulp.src(html).pipe(gulp.dest("./public"));
-
-  return new Promise((resolve, reject) => {
-    resolve("html success");
-  });
+  gulp.src(html).pipe(gulp.dest("public"));
 });
+
+gulp.task("default", gulp.parallel(parallelList), "start-sever");
 
 //init sever
 gulp.task("start-server", function (cb) {
-  exec("node server.js", function (err, stdout, stderr) {
+  exec("node server.ts", function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
