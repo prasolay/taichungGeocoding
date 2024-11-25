77e3362cf056e44eed4c10e1fa453ec482a40d2b
prasolay2020 <prasolay2020@gmail.com>
2024-11-25 17:17:27 +0800

    暫時儲存版本

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,6 +1,8 @@
+console.log("進入系統成功!");
+
 import { ol_map } from "./map";
 const mainMap = ol_map;
-console.log("進入系統成功!");
+console.log(ol_map);
 
 // import { sayHello } from "./greet";
 // function showHello(divName: string, name: string) {
